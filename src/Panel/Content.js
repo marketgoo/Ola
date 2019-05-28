8ed30e55ecf1558456733d909d75152c01bf34bb
Pedro J. Parra <pedro@marketgoo.com>
2019-05-28 16:34:39 +0200

    Update Panel Content

--- src/Panel/Content.js
+++ src/Panel/Content.js
@@ -3,9 +3,9 @@ import {default as PT} from 'prop-types'
 import cx from 'classnames'
 
 const PanelContent = ({ children, title, htmlTitle, variant, extraClass, ...props }) => {
-  const styles = variant ? cx('panel-content', `is-${variant}`, extraClass) : cx('panel-content', extraClass)
+  // const styles = variant ? cx('panel-content', `is-${variant}`, extraClass) : cx('panel-content', extraClass)
   return (
-    <div className={styles} {...props}>
+    <div className={cx('panel-content', variant && `is-${variant}`, extraClass)} {...props}>
       { title && ( htmlTitle ? <h2 className="panel-subtitle" dangerouslySetInnerHTML={{__html: title}} /> : <h2 className="panel-subtitle">{title}</h2>) }
       <div>
         {children}
