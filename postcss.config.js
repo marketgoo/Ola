d155076182e54e6180e897523f0766f7ba542f0e
Oscar Otero <oscar@marketgoo.com>
2019-05-30 15:53:33 +0200

    share browserlist configuration between postcss and babel

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
     'postcss-import': {},
     'postcss-url': {},
     'postcss-preset-env': {
-      browser: '> 0.5%, last 2 versions, Firefox ESR, not dead, not ie',
       stage: 0,
       features: {
         'custom-properties': false
