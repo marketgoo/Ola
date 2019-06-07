c675a8dc48b69e559dbfbb5eb4a497b58fcc520f
Oscar Otero <oscar@marketgoo.com>
2019-06-07 18:17:28 +0200

    styles modal

--- stories/modal.story.js
+++ stories/modal.story.js
@@ -16,16 +16,20 @@ storiesOf('Modal', module)
   .addDecorator(withInfo)
   .addDecorator(story => <div className="ola preview-centered is-wide">{story()}</div>)
   .add('All elements', () => (
-    <Modal>
-      <ModalHeader title="Modal Header" intro="Lorem ipsum for testing intro" />
-      <ModalContent>
-        <p>Modal content</p>
-      </ModalContent>
-      <ModalFooter>
-        <ButtonGroup reversed>
-          <Button variant='primary'>Primary</Button>
-          <Button variant='secondary'>Default Button</Button>
-        </ButtonGroup>
-      </ModalFooter>
-    </Modal>
+    <div className="ola_modal-overlay">
+      <div>
+        <Modal>
+          <ModalHeader title="Modal Header" intro="Lorem ipsum for testing intro" />
+          <ModalContent>
+            <p>Modal content</p>
+          </ModalContent>
+          <ModalFooter>
+            <ButtonGroup reversed>
+              <Button variant='primary'>Primary</Button>
+              <Button variant='secondary'>Default Button</Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </Modal>
+      </div>
+    </div>
   ))
