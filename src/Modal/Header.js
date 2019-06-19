6bf04fc338840a2c17cced81381e065ef9456d47
Pedro J. Parra <pedro@marketgoo.com>
2019-06-19 14:14:51 +0200

    Not render Intro in panel and modal if prop intro is undefined

--- src/Modal/Header.js
+++ src/Modal/Header.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import {default as PT} from 'prop-types'
 
+const ModalTitle = ({ title, htmlTitle=false }) => {
+  return htmlTitle ?
+    <h2 className="ola_modal-title" dangerouslySetInnerHTML={{__html: title}} /> :
+    <h2 className="ola_modal-title">{title}</h2>
+}
+
+const ModalIntro = ({ intro, htmlIntro=false }) => {
+  return htmlIntro ?
+    <p className="ola_modal-intro" dangerouslySetInnerHTML={{__html: intro}} /> :
+    <p className="ola_modal-intro">{intro}</p>
+}
+
 const ModalHeader = ({ title, htmlTitle, intro, htmlIntro, ...props }) => {
   return (
     <div className="ola_modal-header" {...props}>
-      { htmlTitle ?  <h2 className="ola_modal-title" dangerouslySetInnerHTML={{__html: title}} /> : <h2 className="ola_modal-title">{title}</h2> }
-      { htmlIntro ? <p className="ola_modal-intro" dangerouslySetInnerHTML={{__html: intro}} /> : <p className="ola_modal-intro">{intro}</p> }
+      <ModalTitle title={title} htmlTitle={htmlTitle} />
+      { intro && <ModalIntro intro={intro} htmlIntro={htmlIntro} /> }
     </div>
   )
 }
--- src/Panel/Header.js
+++ src/Panel/Header.js
@@ -2,11 +2,23 @@ import React from 'react'
 import {default as PT} from 'prop-types'
 import cx from 'classnames'
 
+const PanelTitle = ({ title, htmlTitle=false }) => {
+  return htmlTitle ?
+    <h1 className="ola_panel-title" dangerouslySetInnerHTML={{__html: title}} /> :
+    <h1 className="ola_panel-title">{title}</h1>
+}
+
+const PanelIntro = ({ intro, htmlIntro=false }) => {
+  return htmlIntro ?
+    <p className="ola_panel-intro" dangerouslySetInnerHTML={{__html: intro}} /> :
+    <p className="ola_panel-intro">{intro}</p>
+}
+
 const PanelHeader = ({ title, htmlTitle, intro, htmlIntro, children }) => {
   return (
     <header className={cx('ola_panel-header', children && 'has-extra')}>
-      { htmlTitle ?  <h1 className="ola_panel-title" dangerouslySetInnerHTML={{__html: title}} /> : <h1 className="ola_panel-title">{title}</h1> }
-      { htmlIntro ? <p className="ola_panel-intro" dangerouslySetInnerHTML={{__html: intro}} /> : <p className="ola_panel-intro">{intro}</p> }
+      <PanelTitle title={title} htmlTitle={htmlTitle} />
+      { intro && <PanelIntro intro={intro} htmlIntro={htmlIntro} /> }
       { children && <div className="ola_panel-extra">{ children }</div> }
     </header>
   )
