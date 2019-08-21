84b94e525d152cd5d06ab2606cf3e7021357b062
Pedro J. Parra <pedro@marketgoo.com>
2019-08-21 16:56:45 +0200

    Update ProgressBar component

--- src/ProgressBar/index.js
+++ src/ProgressBar/index.js
@@ -3,22 +3,40 @@ import {default as PT} from 'prop-types'
 import cx from 'classnames'
 import { getElementType } from '../utils'
 
-const ProgressBar = ({ extraClass, ...props }) => {
+const ProgressBarDescription = ({ description, htmlDescription=false }) => {
+  return htmlDescription ?
+    <p dangerouslySetInnerHTML={{__html: description}} /> :
+    <p>{description}</p>
+}
+
+const ProgressBar = ({ extraClass, description, descriptionPosition, htmlDescription, ...props }) => {
   const ElementType = getElementType(ProgressBar, {...props})
   return (
-    <ElementType className={cx('ola_progressBar', extraClass)} {...props} />
+    <div className={cx('ola_progressBar', `is-description-${descriptionPosition}`, extraClass)}>
+      { description && <ProgressBarDescription description={description} htmlDescription={htmlDescription} /> }
+      <ElementType {...props} />
+    </div>
   )
 }
 
 ProgressBar.defaultProps = {
   as: 'progress',
-  extraClass: null
+  extraClass: null,
+  description: null,
+  descriptionPosition: 'top',
+  htmlDescription: false
 }
 
 ProgressBar.propTypes = {
   as: PT.oneOf(['progress', 'meter']),
   /** Extra className */
-  extraClass: PT.string
+  extraClass: PT.string,
+  /** Description */
+  description: PT.string,
+  /** Description Position */
+  descriptionPosition: PT.oneOf(['top', 'right']),
+  /** Description support HTML tags */
+  htmlDescription: PT.bool
 }
 
 export { ProgressBar }
--- stories/progressBar.stories.js
+++ stories/progressBar.stories.js
@@ -10,10 +10,14 @@ storiesOf('Progress Bar', module)
   .add('Progress element', () => (
     <div className="preview-centered-inner">
       <ProgressBar value="20" max="100" />
+      <ProgressBar value="50" max="100" description="Loading..." />
+      <ProgressBar value="80" max="100" htmlDescription description="<strong>20%</strong> completed" descriptionPosition="right" />
     </div>
   ))
   .add('Meter element', () => (
     <div className="preview-centered-inner">
-      <ProgressBar as="meter" value="160" min="100" max="200" />
+      <ProgressBar as="meter" value="120" min="100" max="200" low="130" />
+      <ProgressBar as="meter" value="150" min="100" max="200" description="150 / 200" />
+      <ProgressBar as="meter" value="180" min="100" max="200" htmlDescription description="<strong>180 / 200</strong>" descriptionPosition="right" />
     </div>
   ))
