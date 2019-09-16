59ff12b7fef6cae40bea7f2af5d733536ccbf50e
Oscar Otero <oscar@marketgoo.com>
2019-09-16 12:41:11 +0200

    simplificados los nombres de las utilidades .ola_utils-* para .ola-*

--- stories/utils.stories.js
+++ stories/utils.stories.js
@@ -6,12 +6,12 @@ storiesOf('Utils', module)
   .addDecorator(withInfo)
   .addDecorator(story => <div className="ola preview-centered is-wide">{story()}</div>)
   .add('Font styles', () => (
-    <div>
-      <div className="ola_util-display">Display</div>
-      <div className="ola_util-title">Title</div>
-      <div className="ola_util-headline">Headline</div>
-      <div className="ola_util-body">Body</div>
-      <div className="ola_util-callout">Callout</div>
-      <div className="ola_util-caption">Caption</div>
+    <div class="ola-bg">
+      <div className="ola-display">Display</div>
+      <div className="ola-title">Title</div>
+      <div className="ola-headline">Headline</div>
+      <div className="ola-body">Body</div>
+      <div className="ola-callout">Callout</div>
+      <div className="ola-caption">Caption</div>
     </div>
   ))
