20f49fe66a42914ea39885d66c62d67fe59d5908
Oscar Otero <oscar@marketgoo.com>
2019-11-27 19:35:02 +0100

    fixes in blink

--- src/Accordion/index.js
+++ src/Accordion/index.js
@@ -8,9 +8,9 @@ const Accordion = ({ children, title, extraClass, ...props }) => {
   const styles = cx('ola_accordion', extraClass)
   return (
     <details className={styles} {...props}>
-        <summary className="ola_accordion-title">
+        <summary className="ola_accordion-header">
             <span className="ola_accordion-marker"><Icon name="chevronRight" /></span>
-            { title }
+            <span className="ola_accordion-title">{ title }</span>
         </summary>
         <div className="ola_accordion-content">
             { children }
--- stories/accordion.stories.js
+++ stories/accordion.stories.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import { withInfo } from '@storybook/addon-info'
-import { storiesOf } from '@storybook/react'
-import { withKnobs, radios } from '@storybook/addon-knobs'
+
+import create from './create'
 
 import { Accordion } from '../dist'
 import { Tag } from '../dist'
 
-storiesOf('Accordion', module)
-  .addDecorator(withKnobs)
-  .addDecorator(withInfo)
-  .addDecorator(story => <div className="ola">{story()}</div>)
+create('Accordion')
   .add('Default', () => (
-    <Accordion title={<>
+    <Accordion title={
+      <>
         This is the title
         <Tag>23</Tag>
-        </>}>
+      </>
+    }>
         <p>Lorem ipsum</p>
     </Accordion>
   ))
