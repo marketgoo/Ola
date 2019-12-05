70b5ee3bd7070ec9c38e670fd508015b7bf10c82
Oscar Otero <oscar@marketgoo.com>
2019-12-05 11:25:32 +0100

    css styles for selectable TableRows #38

---
+++ src/Table/Row.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {default as PT} from 'prop-types'
+import cx from 'classnames'
+import {TableCell} from './Cell.js'
+
+const TableRow = ({ extraClass, select, children, ...props }) => {
+  return (
+    <tr className={cx('ola_tableRow', {'is-selectable': select}, extraClass)} {...props}>
+      {select && (<TableCell>{select}</TableCell>)}
+      {children}
+    </tr>
+  )
+}
+
+TableRow.defaultProps = {
+
+}
+
+TableRow.propTypes = {
+  /** Extra className */
+  extraClass: PT.string,
+  /** Selectable check */
+  select: PT.node,
+  /** Childen nodes */
+  children: PT.oneOfType([
+    PT.string,
+    PT.arrayOf(PT.node),
+    PT.node
+  ])
+}
+
+export { TableRow }
