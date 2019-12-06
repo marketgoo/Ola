a75bcf30b63df1c8b0734411f926391c7e887452
Pedro J. Parra <pedro@marketgoo.com>
2019-12-06 09:49:05 +0100

    Add Field description test

---
+++ src/FieldDescription/test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FieldDescription } from './'
+import renderer from 'react-test-renderer'
+
+it('Default FieldDescription', () => {
+  const tree = renderer
+    .create(<FieldDescription>Description</FieldDescription>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('Default FieldDescription with error', () => {
+  const tree = renderer
+    .create(<FieldDescription error={true}>Description</FieldDescription>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
