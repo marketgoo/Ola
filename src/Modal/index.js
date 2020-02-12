82cea5882d9ba4446f1c36dd4b682bc908e4a921
Diego Pascual <diego@marketgoo.com>
2020-02-12 15:36:21 +0100

    proposed changes

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -3,34 +3,33 @@ import {default as PT} from 'prop-types'
 import cx from 'classnames'
 import Icon from '../Icon'
 
-const Overlay = ({ children, open, ...props }) => open ?
-  <div className="ola_modal-overlay" {...props}>
-    {children}
-  </div> : null
-
-const Modal = ({ extraClass, children, closeWhenClickOut = true, closeWhenPressEsc = true, onClose, ...props }) => {
+const Modal = ({ extraClass, children, open, onClose, ...props }) => {
     
-  useEffect(() => closeWhenPressEsc && document.body.addEventListener('keydown', e => e.keyCode === 27 && onClose()), [])
+  useEffect(() => {
+    if (open){
+      document.body.addEventListener('keydown', e => e.keyCode === 27 && onClose())
+      return () => document.body.removeEventListener('keydown')
+    }
+  },[])
 
-  return (
-    <Overlay onClick={closeWhenClickOut && onClose} {...props}>
-      <dialog onClick={e => e.stopPropagation()} className={cx('ola_modal', extraClass)} {...props}>
-        <button className="ola_modal-close ola_buttonIcon" onClick={onClose}>
-          <Icon name="close" />
-        </button>
-        {children}
-      </dialog>
-    </Overlay>
-  )
+  if (open){
+    return (
+      <div className="ola_modal-overlay" onClick={onClose}>
+        <dialog onClick={e => e.stopPropagation()} className={cx('ola_modal', extraClass)} open={open} {...props}>
+          <button className="ola_modal-close ola_buttonIcon" onClick={onClose}>
+            <Icon name="close" />
+          </button>
+          {children}
+        </dialog>
+      </div>
+    )
+  }
+  return null
 }
 
 Modal.propTypes = {
   /** Open or close Modal */
   open: PT.bool,
-  /** Close Modal when click out. Default is true */
-  closeWhenClickOut: PT.bool,
-  /** Close Modal when press ESC. Default is true */
-  closeWhenPressEsc: PT.bool,
   /** On Close function */
   onClose: PT.func,
   /** Extra className */
