74631f56d8faaf9f94b17031f571888dc89fa979
Oscar Otero <oscar@marketgoo.com>
2020-02-19 13:50:55 +0100

    Added extraClass and variants to tooltips

--- src/Tooltip/stories.js
+++ src/Tooltip/stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { radios } from '@storybook/addon-knobs'
 
 import Tooltip from './'
 import Button from '../Button'
@@ -20,5 +21,16 @@ storiesOf('Tooltip')
   .add('With ButtonIcon', () => (
     <figure>
       <Tooltip trigger={<ButtonIcon as="span"><Icon name="help" /></ButtonIcon>}>Lorem ipsum</Tooltip>
+    </figure>
+  ))
+  .add('Variants', () => (
+    <figure>
+      <Tooltip variant={radios('Variant', ['', 'wide'], '')} trigger={<ButtonIcon as="span"><Icon name="help" /></ButtonIcon>}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Quisque ligula lacus, faucibus vitae sodales a, eleifend in massa.
+        Donec sapien massa, interdum et facilisis vitae, elementum ac orci.
+        Nam id blandit turpis. Cras eleifend accumsan nisi vitae luctus.
+        Donec ultricies auctor sodales.
+      </Tooltip>
     </figure>
   ))
