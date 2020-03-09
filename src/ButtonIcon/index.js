2d2857819331f9feeb096f61c264a595c9776dd5
Oscar Otero <oscar@marketgoo.com>
2020-03-09 16:02:25 +0100

    added variants to buttonIcon

--- src/ButtonIcon/index.js
+++ src/ButtonIcon/index.js
@@ -3,9 +3,15 @@ import {default as PT} from 'prop-types'
 import { getElementType } from '../utils'
 import cx from 'classnames'
 
-const ButtonIcon = ({ as, extraClass, children, ...props }) => {
+const ButtonIcon = ({ as, extraClass, children, variant, ...props }) => {
   const ElementType = getElementType(ButtonIcon, { as: as, ...props })
-  return (<ElementType {...props} className={cx('ola_buttonIcon', extraClass)}>{children}</ElementType>)
+  delete props['as']
+  const styles = cx(
+    'ola_buttonIcon',
+    {[`is-${variant}`]: variant },
+    extraClass
+  )
+  return (<ElementType {...props} className={styles}>{children}</ElementType>)
 }
 
 ButtonIcon.defaultProps = {
@@ -15,6 +21,8 @@ ButtonIcon.defaultProps = {
 ButtonIcon.propTypes = {
   /** Render ButtonIcon with any html tag */
   as: PT.string,
+  /** Button variants */
+  variant: PT.oneOf(['normal', 'destructive']),
   /** Extra className */
   extraClass: PT.string,
   /** Childen nodes */
