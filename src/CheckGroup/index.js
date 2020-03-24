089dbf7a26fae0e015c852db5ccd06297f2efdd4
Pedro J. Parra <pedro@marketgoo.com>
2020-03-24 11:43:36 +0100

    This fix #77

--- src/CheckGroup/index.js
+++ src/CheckGroup/index.js
@@ -3,6 +3,8 @@ import {default as PT} from 'prop-types'
 import cx from 'classnames'
 
 const CheckGroup = ({ variant, children, className, ...props }) => {
+  // Delete any error prop inherited from Field
+  if(props.error) delete props['error'] // eslint-disable-line react/prop-types
   return (
     <div role="radiogroup" className={cx('ola_checkGroup', variant && `is-${variant}`, className)} { ...props }>
       { children }
