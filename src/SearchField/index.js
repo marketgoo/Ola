80f03db7cb9a4cbd0420f3479905727118cae63b
Oscar Otero <oom@oscarotero.com>
2020-04-28 16:45:40 +0200

    improved SearchField

--- src/SearchField/index.js
+++ src/SearchField/index.js
@@ -3,23 +3,30 @@ import {default as PT} from 'prop-types'
 
 import Button from '../Button'
 import Input from '../Input'
+import Icon from '../Icon'
+import ButtonIcon from '../ButtonIcon'
 import Spinner from '../Spinner'
 
-const SearchField = ({ id, textButton, busy, placeholder, ...props }) => {
+const SearchField = ({ id, textButton, busy, placeholder, description, ...props }) => {
   return (
     <div className="ola_searchField">
-      <div className="ola_searchField-field">
-        <label htmlFor={id} className="ola_searchField-label">{ placeholder }</label>
-        <Input type="search" id={id} className="ola_searchField-input" placeholder={placeholder} {...props} />
-        {busy && <Spinner />}
+      <div className="ola_searchField-form">
+        <div className="ola_searchField-field">
+          <label htmlFor={id} className="ola_searchField-label">{ placeholder }</label>
+          <Input type="search" id={id} className="ola_searchField-input" placeholder={placeholder} {...props} />
+          <ButtonIcon tabIndex="-1" variant="secondary" className="ola_searchField-icon"><Icon name="search" /></ButtonIcon>
+          {busy && <Spinner />}
+        </div>
+        { textButton && <Button variant="primary" disabled busy={busy && textButton} className="ola_searchField-button" tabIndex="-1">{ textButton }</Button> }
       </div>
-      { textButton && <Button variant="primary" busy={busy && textButton} className="ola_searchField-button" tabIndex="-1">{ textButton }</Button> }
+      { description && <p className="ola_searchField-description">{description}</p> }
     </div>
   )
 }
 
 SearchField.defaultProps = {
   textButton: null,
+  description: null,
   busy: false
 }
 
@@ -30,6 +37,12 @@ SearchField.propTypes = {
   textButton: PT.string,
   /** Placeholder ( required because works like label )*/
   placeholder: PT.string.isRequired,
+  /** Description */
+  description: PT.oneOfType([
+    PT.arrayOf(PT.node),
+    PT.node,
+    PT.string
+  ]),
   /** Busy */
   busy: PT.bool
 }
--- src/SearchField/stories.js
+++ src/SearchField/stories.js
@@ -10,7 +10,7 @@ storiesOf('SearchField')
   .add('Busy with button', () => (
     <figure><SearchField id="search-loading" busy placeholder="Filter elements" textButton="Test Search" /></figure>
   ))
-  .add('Default', () => (
-    <figure><SearchField id="search-button" placeholder="Filter elements" textButton="Test Search" /></figure>
+  .add('Default with description', () => (
+    <figure><SearchField id="search-button" placeholder="Filter elements" textButton="Test Search" description="Use commas to search for more than one keyword" /></figure>
   ))
   
