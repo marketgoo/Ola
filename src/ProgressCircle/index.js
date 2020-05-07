d4d4999fb837800d721d5bfd2dddd94cfe4c6f97
Pedro J. Parra <pedro@marketgoo.com>
2020-05-07 11:18:03 +0200

    Now we can add content when ProgressCircle is busy #125

--- src/ProgressCircle/index.js
+++ src/ProgressCircle/index.js
@@ -8,16 +8,28 @@ const progressCircleValue = (value) => {
   return ((100 - value) / 100) * diameter
 }
 
+const showBusyContent = (busy) => {
+  if(busy) {
+    const busyIsNode = React.isValidElement(busy)
+    const busyIsString = typeof(busy) === 'string'
+    return busyIsNode || busyIsString
+  }
+  return false
+}
+
 const ProgressCircle = ({ variant, value, busy, label, className, ...props }) => {
 
   const circleStyle = {
     strokeDashoffset: progressCircleValue(value)
   }
 
+
+
   return (
     <div className={cx('ola_progressCircle', variant && `is-${variant}`, {'is-busy': busy}, className)} {...props}>
-      { label &&  <span className="ola_progressCircle-label">{label}</span> }
-      { (value !== null) &&  <strong className="ola_progressCircle-value">{value}<span>%</span></strong> }
+      { !busy && label &&  <span className="ola_progressCircle-label">{label}</span> }
+      { !busy && (value !== null) &&  <strong className="ola_progressCircle-value">{value}<span>%</span></strong> }
+      { showBusyContent(busy) && <span className="ola_progressCircle-busy">{busy}</span> }
       <svg className="ola_progressCircle-circle" width="140" height="140" viewBox="0 0 140 140">
         <circle r="62" cx="70" cy="70" className="ola_progressCircle-circle-background"></circle>
         <circle r="62" cx="70" cy="70" className="ola_progressCircle-circle-value" style={circleStyle}></circle>
@@ -41,7 +53,10 @@ ProgressCircle.propTypes = {
   /** Value of progress */
   value: PT.number,
   /** Busy or loading */
-  busy: PT.bool,
+  busy: PT.oneOfType([
+    PT.string,
+    PT.bool
+  ]),
   /** Extra className */
   className: PT.string,
   /** Label */
