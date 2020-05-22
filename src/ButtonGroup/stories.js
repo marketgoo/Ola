a57106c29fe949461bbd4aafafd1915f95373d2c
Oscar Otero <oom@oscarotero.com>
2020-05-22 17:07:23 +0200

    Added narrow and reversed-narrow variants to ButtonGroup

--- src/ButtonGroup/stories.js
+++ src/ButtonGroup/stories.js
@@ -8,7 +8,14 @@ import ButtonGroup from './'
 
 storiesOf('ButtonGroup')
   .add('Viewer', () => (
-    <ButtonGroup variant={radios('Variant', ['default', 'center', 'reversed', 'reversed-center'], 'default')} style={{width: '500px', border: 'solid 1px'}}>
+    <ButtonGroup variant={radios('Variant', [
+      'default',
+      'center',
+      'reversed',
+      'reversed-center',
+      'narrow',
+      'reversed-narrow'
+    ], 'default')} style={{width: '500px', border: 'solid 1px'}}>
       <Button variant='primary' onClick={action('onClick event')}>Accept</Button>
       <Button variant='secondary' onClick={action('onClick event')}>Cancel</Button>
     </ButtonGroup>
