351bf160df2ed3188a9d49541f2a157499b7a3e9
mablanco <mblanco@marketgoo.com>
2020-07-10 15:22:19 +0200

    fix formatter

--- src/Field/index.js
+++ src/Field/index.js
@@ -2,16 +2,7 @@ import React from "react";
 import { default as PT } from "prop-types";
 import cx from "classnames";
 
-const Field = ({
-  id,
-  label,
-  hint,
-  error,
-  description,
-  disabled,
-  children,
-  maxCharacter,
-}) => {
+const Field = ({id,label,hint,error,description,disabled,children,maxCharacter,}) => {
   let str;
   const handleContent = (ev) => {
     if (maxCharacter) {
@@ -26,64 +17,27 @@ const Field = ({
   if (maxCharacter) {
     return (
       <div className={cx("ola_field", { error }, { "is-disabled": disabled })}>
-        <label htmlFor={id} className="ola_field-label">
-          {hint && (
-            <span className="ola_field-hint">
-              hola
-            </span>
-          )}
+        <label htmlFor={id} className="ola_field-label"> {hint && (<span className="ola_field-hint">hola </span>)}
         </label>
         <div className="ola_field-input">
-          {React.cloneElement(children, {
-            id: id,
-            error,
-            disabled,
-            onChange: handleContent,
-          })}
+          {React.cloneElement(children, {id: id,error,disabled,onChange: handleContent,})}
         </div>
-        {description && (
-          <p
-            className={cx({
-              "ola_field-error": error,
-              "ola_field-description": !error,
-            })}
-          >
-            {description}
-          </p>
+        {description && (<p className={cx({"ola_field-error": error,"ola_field-description": !error,})}>{description}</p>
         )}
       </div>
     );
   }
   return (
     <div
-      className={cx(
-        "ola_field",
-        { "is-invalid": error },
-        { "is-disabled": disabled }
-      )}
-    >
+      className={cx("ola_field",{ "is-invalid": error },{ "is-disabled": disabled })}>
       <label htmlFor={id} className="ola_field-label">
         {label}
         {hint && <span className="ola_field-hint">{hint}</span>}
       </label>
       <div className="ola_field-input">
-        {React.cloneElement(children, {
-          id: id,
-          error,
-          disabled,
-          onChange: handleContent,
-        })}
+        {React.cloneElement(children, {id: id,error,disabled,onChange: handleContent,})}
       </div>
-      {description && (
-        <p
-          className={cx({
-            "ola_field-error": error,
-            "ola_field-description": !error,
-          })}
-        >
-          {description}
-        </p>
-      )}
+      {description && (<p className={cx({"ola_field-error": error,"ola_field-description": !error,})}>{description}</p>)}
     </div>
   );
 };
