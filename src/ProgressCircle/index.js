b7c4627bd55afeef579c8e0e67630dc9ab72ee65
Diego Pascual <diego@marketgoo.com>
2020-08-03 12:40:19 +0200

    feat: add description prop to ProgressCircle

--- src/ProgressCircle/index.js
+++ src/ProgressCircle/index.js
@@ -17,18 +17,17 @@ const showBusyContent = (busy) => {
   return false
 }
 
-const ProgressCircle = ({ variant, value, busy, label, className, ...props }) => {
+const ProgressCircle = ({ variant, value, description, busy, label, className, ...props }) => {
 
   const circleStyle = {
     strokeDashoffset: progressCircleValue(value)
   }
 
-
-
   return (
     <div className={cx('ola_progressCircle', variant && `is-${variant}`, {'is-busy': busy}, className)} {...props}>
-      { !busy && label &&  <span className="ola_progressCircle-label">{label}</span> }
+      { !busy && label && <span className="ola_progressCircle-label">{label}</span> }
       { !busy && (value !== null) &&  <strong className="ola_progressCircle-value">{value}<span>%</span></strong> }
+      { !busy && value !== null && description !== null && (typeof description === 'string' ? <span className="ola_progressCircle-label">{description}</span> : description) }
       { showBusyContent(busy) && <span className="ola_progressCircle-busy">{busy}</span> }
       <svg className="ola_progressCircle-circle" width="140" height="140" viewBox="0 0 140 140">
         <circle r="62" cx="70" cy="70" className="ola_progressCircle-circle-background"></circle>
@@ -42,6 +41,7 @@ const ProgressCircle = ({ variant, value, busy, label, className, ...props }) =>
 ProgressCircle.defaultProps = {
   variant: null,
   value: null,
+  description: null,
   label: null,
   busy: false,
   className: null
@@ -52,6 +52,8 @@ ProgressCircle.propTypes = {
   variant: PT.oneOf(['error', 'success', 'warning']),
   /** Value of progress */
   value: PT.number,
+  /** Description of progress */
+  description: PT.oneOfType[PT.string, PT.node],
   /** Busy or loading */
   busy: PT.oneOfType([
     PT.string,
