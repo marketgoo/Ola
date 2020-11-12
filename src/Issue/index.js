47dbbb73f26463be20744f2fccf1d90a6dd1d636
Diego Pascual <diego@marketgoo.com>
2020-11-12 17:01:46 +0100

    feat: improvements

--- src/Issue/index.js
+++ src/Issue/index.js
@@ -2,31 +2,32 @@ import React from 'react'
 import { default as PT } from 'prop-types'
 import Icon from '../Icon'
 
-const IssueIcon = ({ variant }) =>
+const IssueIcon = ({ variant, size }) =>
   ({
-    success: <Icon name="success" className="ola_issue-status" />,
-    warning: <Icon name="warning" className="ola_issue-status" />,
-    suggested: <Icon name="help" className="ola_issue-status" />,
-  }[variant] || <Icon name="error" className="ola_issue-status" />)
+    success: <Icon name="success" size={size} className="ola_issue-status" />,
+    warning: <Icon name="warning" size={size} className="ola_issue-status" />,
+    suggested: <Icon name="help" size={size} className="ola_issue-status" />,
+  }[variant] || <Icon name="error" size={size} className="ola_issue-status" />)
 
-const IssueTitle = ({ title, link }) => (
+const IssueTitle = ({ title, link, size }) => (
   <>
-    <span className="ola_issue-title">{title}</span>
+    <span className={'ola_issue-title ola-bold ' + (size === 'small' ? 'ola-callout' : 'ola-body')}>{title}</span>
     {link && <Icon name="chevronRight" />}
   </>
 )
 
-const Issue = ({ title, variant, link }) => (
-  <a href={link} className={`ola_issue is-${variant}`}>
+const Issue = ({ title, variant, link, size }) => (
+  <div href={link} className={`ola_issue is-${variant}`}>
     <div className="ola_issue-header">
-      <IssueIcon variant={variant} />
-      <IssueTitle title={title} link={link} />
+      <IssueIcon variant={variant} size={size} />
+      <IssueTitle title={title} link={link} size={size} />
     </div>
-  </a>
+  </div>
 )
 
 Issue.defaultProps = {
   variant: 'error',
+  size: 'medium'
 }
 
 Issue.propTypes = {
@@ -36,6 +37,8 @@ Issue.propTypes = {
   title: PT.oneOfType([PT.string, PT.arrayOf(PT.node), PT.node]).isRequired,
   /** Title right component  */
   link: PT.string,
+  /** Size variant */
+  size: PT.oneOf(['small', 'medium'])
 }
 
 export default Issue
