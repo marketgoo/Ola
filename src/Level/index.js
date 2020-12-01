2e3b003a80000b3870d4815df570835989007a0b
Oscar Otero <oom@oscarotero.com>
2020-12-01 17:49:15 +0100

    Fix #169

--- src/Level/index.js
+++ src/Level/index.js
@@ -2,20 +2,28 @@ import React from 'react'
 import {default as PT} from 'prop-types'
 import cx from 'classnames'
 
-const Level = ({ variant }) => {
-  const styles = cx('ola_level', variant && `is-${variant}`)
+const Level = ({ variant, size }) => {
+  const styles = cx(
+    'ola_level',
+    variant && `is-${variant}`,
+    size === "medium" ? null : `is-${size}`
+  )
+
   return (
     <span className={styles}></span>
   )
 }
 
 Level.defaultProps = {
-  variant: null
+  variant: null,
+  size: 'medium',
 }
 
 Level.propTypes = {
   /** Level variants */
   variant: PT.oneOf(['low', 'medium', 'high']),
+  /** Level sizes */
+  size: PT.oneOf(['small', 'medium']),
 }
 
 export default Level
