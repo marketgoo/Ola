f54ad95039aa97236de09a638d91be82c4ef5f29
Oscar Otero <oom@oscarotero.com>
2021-02-12 13:52:57 +0100

    improved thumbnail

--- src/Thumbnail/index.js
+++ src/Thumbnail/index.js
@@ -2,10 +2,20 @@ import React from 'react'
 import {default as PT} from 'prop-types'
 import cx from 'classnames'
 
-const Thumbnail = ({ className, url, borderColor }) => {
+const Thumbnail = ({ className, url, borderColor, children }) => {
   return (
     <div className={ cx( 'ola_thumbnail', className )} style={{'--color': borderColor}}>
-      <img className={'ola_thumbnail-image'} src={url} />
+      {
+        url
+          ? <img className={'ola_thumbnail-image'} src={url} />
+          : <svg className={'ola_thumbnail-image'} width="400" height="300" viewBox="0 0 400 300">
+            <path d="M270 94a8 8 0 018 8v94a8 8 0 01-8 8H130a8 8 0 01-8-8v-94a8 8 0 018-8h140zm6 22H124v79a7 7 0 007 7h138a7 7 0 007-7v-79z" />
+            <circle cx="140" cy="105" r="5"/>
+            <circle cx="159" cy="105" r="5"/>
+            <circle cx="178" cy="105" r="5"/>
+          </svg>
+      }
+      { children }
     </div>
   )
 }
@@ -27,4 +37,10 @@ Thumbnail.propTypes = {
   borderColor: PT.string,
   /** Url status */
   status: PT.oneOf(['loaded', 'loading', 'empty']),
+  /** Childen nodes */
+  children: PT.oneOfType([
+    PT.string,
+    PT.arrayOf(PT.node),
+    PT.node
+  ]),
 }
--- src/Thumbnail/stories.js
+++ src/Thumbnail/stories.js
@@ -3,10 +3,11 @@ import Thumbnail from './'
 
 export default {
   title: 'Thumbnail',
+  component: Thumbnail
 }
 
-export const Base = (args) => <Thumbnail {...args} url="https://placehold.it/100x100"/>
-export const Border = (args) => <div style={{ display: 'flex' }}>
+export const Base = (args) => <Thumbnail {...args}/>
+export const Border = (args) => <div style={{ display: 'grid', gap: '20px', 'grid-auto-flow': 'column', 'grid-auto-columns': '100px' }}>
   <Thumbnail {...args} borderColor={'#F48498'} url="https://placehold.it/100x100"/>
   <Thumbnail {...args} borderColor={'#7FD8BE'} url="https://placehold.it/100x100"/>
   <Thumbnail {...args} borderColor={'#FFD266'} url="https://placehold.it/100x100"/>
