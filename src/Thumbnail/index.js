5e5256108d53c854314276d3716f07b3b21f1bd5
Pedro J. Parra <pedro@marketgoo.com>
2021-02-18 13:29:40 +0100

    Add onError event to thumbnail

--- src/Thumbnail/index.js
+++ src/Thumbnail/index.js
@@ -1,21 +1,34 @@
-import React from 'react'
-import {default as PT} from 'prop-types'
+import React, { useState } from 'react'
+import { default as PT } from 'prop-types'
 import cx from 'classnames'
 
 const Thumbnail = ({ className, url, borderColor, children }) => {
+  const [thumbnailUrl, setThumbnailUrl] = useState(url)
   return (
-    <div className={ cx( 'ola_thumbnail', className )} style={{'--color': borderColor}}>
-      {
-        url
-          ? <img className={'ola_thumbnail-image'} src={url} />
-          : <svg className={'ola_thumbnail-image'} width="400" height="300" viewBox="0 0 400 300">
-            <path d="M270 94a8 8 0 018 8v94a8 8 0 01-8 8H130a8 8 0 01-8-8v-94a8 8 0 018-8h140zm6 22H124v79a7 7 0 007 7h138a7 7 0 007-7v-79z" />
-            <circle cx="140" cy="105" r="5"/>
-            <circle cx="159" cy="105" r="5"/>
-            <circle cx="178" cy="105" r="5"/>
-          </svg>
-      }
-      { children }
+    <div
+      className={cx('ola_thumbnail', className)}
+      style={{ '--color': borderColor }}
+    >
+      {thumbnailUrl ? (
+        <img
+          className={'ola_thumbnail-image'}
+          src={url}
+          onError={() => setThumbnailUrl(null)}
+        />
+      ) : (
+        <svg
+          className={'ola_thumbnail-image'}
+          width="400"
+          height="300"
+          viewBox="0 0 400 300"
+        >
+          <path d="M270 94a8 8 0 018 8v94a8 8 0 01-8 8H130a8 8 0 01-8-8v-94a8 8 0 018-8h140zm6 22H124v79a7 7 0 007 7h138a7 7 0 007-7v-79z" />
+          <circle cx="140" cy="105" r="5" />
+          <circle cx="159" cy="105" r="5" />
+          <circle cx="178" cy="105" r="5" />
+        </svg>
+      )}
+      {children}
     </div>
   )
 }
@@ -25,7 +38,7 @@ export default Thumbnail
 Thumbnail.defaultProps = {
   className: null,
   status: 'loaded',
-  borderColor: null
+  borderColor: null,
 }
 
 Thumbnail.propTypes = {
@@ -38,9 +51,5 @@ Thumbnail.propTypes = {
   /** Url status */
   status: PT.oneOf(['loaded', 'loading', 'empty']),
   /** Childen nodes */
-  children: PT.oneOfType([
-    PT.string,
-    PT.arrayOf(PT.node),
-    PT.node
-  ]),
+  children: PT.oneOfType([PT.string, PT.arrayOf(PT.node), PT.node]),
 }
--- src/Thumbnail/stories.js
+++ src/Thumbnail/stories.js
@@ -3,13 +3,42 @@ import Thumbnail from './'
 
 export default {
   title: 'Thumbnail',
-  component: Thumbnail
+  component: Thumbnail,
 }
 
-export const Base = (args) => <Thumbnail {...args}/>
-export const Border = (args) => <div style={{ display: 'grid', gap: '20px', 'grid-auto-flow': 'column', 'grid-auto-columns': '100px' }}>
-  <Thumbnail {...args} borderColor={'#F48498'} url="https://placehold.it/100x100"/>
-  <Thumbnail {...args} borderColor={'#7FD8BE'} url="https://placehold.it/100x100"/>
-  <Thumbnail {...args} borderColor={'#FFD266'} url="https://placehold.it/100x100"/>
-  <Thumbnail {...args} borderColor={'#95B8D1'} url="https://placehold.it/100x100"/>
-</div>
+export const Base = (args) => <Thumbnail {...args} />
+export const Border = (args) => (
+  <div
+    style={{
+      display: 'grid',
+      gap: '20px',
+      'grid-auto-flow': 'column',
+      'grid-auto-columns': '100px',
+    }}
+  >
+    <Thumbnail
+      {...args}
+      borderColor={'#F48498'}
+      url="https://placehold.it/100x100"
+    />
+    <Thumbnail
+      {...args}
+      borderColor={'#7FD8BE'}
+      url="https://placehold.it/100x100"
+    />
+    <Thumbnail
+      {...args}
+      borderColor={'#FFD266'}
+      url="https://placehold.it/100x100"
+    />
+    <Thumbnail
+      {...args}
+      borderColor={'#95B8D1'}
+      url="https://placehold.it/100x100"
+    />
+  </div>
+)
+
+export const OnError = (args) => (
+  <Thumbnail {...args} url="https://estaurlnoexisite.it/100x100" />
+)
