6e983a9ae3b94365a536350ed74a80b36d58c6c6
griselmatosm <griselmatosm@gmail.com>
2021-04-13 17:30:03 +0200

    add masks

--- src/SpeedMeter/index.js
+++ src/SpeedMeter/index.js
@@ -3,20 +3,12 @@ import { default as PT } from 'prop-types'
 import cx from 'classnames'
 import Tag from '../Tag'
 
-const scoreValue = (value) => {
-  value = Math.min(30, Math.max(0, value))
-  const diameter = Math.PI * (90 * 2)
-  return ((100 - value) / 100) * diameter
-}
+const diameter = Math.PI * (90 * 2)
+const semi_diameter = diameter / 2
 
-const scoreLevel = (value) => {
-  if (value > 15) {
-    return 'error'
-  }
-  if (value < 7) {
-    return 'success'
-  }
-  return 'warning'
+const scoreValue = (value) => {
+  const meter_value = semi_diameter - ((value * semi_diameter)/ 30)
+  return meter_value
 }
 
 const showBusyContent = (busy) => {
@@ -28,7 +20,7 @@ const showBusyContent = (busy) => {
   return false
 }
 
-const SpeedMeter = ({ value, level, busy, title, className, ...props }) => {
+const SpeedMeter = ({ mark, value, level, busy, title, className, ...props }) => {
   let decimal
 
   if (!Number.isInteger(value)) {
@@ -37,18 +29,23 @@ const SpeedMeter = ({ value, level, busy, title, className, ...props }) => {
   }
 
   const circleStyle = {
-    strokeDashoffset: scoreValue(value)
+    strokeDasharray: scoreValue(value) + ',' + diameter
+  }
+  const positionArrow = {
+    transform: 'rotate(' + (((scoreValue(value) * 180) / 30)) + 'deg)'
   }
 
-  const variant = scoreLevel(value)
-
   return (
-    <div className={cx('ola_speedmeter', `is-${variant}`, { 'is-busy': busy }, className)} {...props}>
+    <div className={cx('ola_speedmeter', { 'is-busy': busy }, className)} {...props}>
       <svg viewBox="0 0 188 94" width="188" height="94">
         <g fillRule="nonzero" fill="none">
-          <path className="ola_speedmeter-circle-background" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" />
-          <path className="ola_speedmeter-circle-value" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" style={circleStyle}/>
-          <path className="ola_speedmeter-triangle" fill="#D8D8D8" d="M94 19.149l10.465 13.404h-20.93z"/>
+          <path className="ola_speedmeter-circle-background" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" strokeDasharray="283, 566" />
+          <path className="ola_speedmeter-circle-value-high" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" stroke="#17AB4D" strokeDasharray="283, 566" />
+          <path className="ola_speedmeter-circle-value-medium" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" stroke="#D7800F" strokeDasharray="189, 566" />
+          <path className="ola_speedmeter-circle-value-low" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" stroke="#DE0220" strokeDasharray="80, 566"/>
+          <path className="ola_speedmeter-circle-mask" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" style={circleStyle}/>
+          <path className="" d="M184 90c0-45.475-40.295-82.34-90-82.34S4 44.525 4 90" strokeDasharray="2, 142" stroke="#F0F2F4" strokeWidth="9"/>
+          <path className="ola_speedmeter-triangle" fill="#D8D8D8" d="M94 19.149l10.465 13.404h-20.93z" style={positionArrow}/>
           <path className="ola_speedmeter-breakpoint" stroke="#979797" strokeWidth="2" fill="#979797" d="M94 0v15.319"/>
         </g>
       </svg>
