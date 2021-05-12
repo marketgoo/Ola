c04f01dca5335470020c4d780498d6c03c5bac2c
griselmatosm <griselmatosm@gmail.com>
2021-05-12 17:51:54 +0200

    Add a four_level type

--- src/Level/index.js
+++ src/Level/index.js
@@ -1,27 +1,38 @@
 import React from 'react'
-import {default as PT} from 'prop-types'
+import { default as PT } from 'prop-types'
 import cx from 'classnames'
 
-const Level = ({ variant, size }) => {
+const Level = ({ type, variant, size }) => {
   const styles = cx(
     'ola_level',
     variant && `is-${variant}`,
-    size === 'medium' ? null : `is-${size}`
+    size === 'medium' ? null : `is-${size}`,
+    `is-${type}`
   )
 
   return (
-    <span className={styles}></span>
+    type === 'three_levels' ?
+      <span className={styles}></span> :
+      <div className={styles}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
   )
 }
 
 Level.defaultProps = {
+  type: 'three_levels',
   variant: null,
   size: 'medium',
 }
 
 Level.propTypes = {
+  /** Level types */
+  type: PT.oneOf(['three_levels', 'four_levels']),
   /** Level variants */
-  variant: PT.oneOf(['low', 'medium', 'high']),
+  variant: PT.oneOf(['low', 'medium', 'high', 'highest']),
   /** Level sizes */
   size: PT.oneOf(['small', 'medium']),
 }
