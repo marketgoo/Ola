63f9b7f82e1cae05b82b9d8a808618e3c01bb0bc
griselmatosm <griselmatosm@gmail.com>
2021-05-12 18:38:28 +0200

    keep 4 levels and some css changes

--- src/Level/index.js
+++ src/Level/index.js
@@ -2,35 +2,29 @@ import React from 'react'
 import { default as PT } from 'prop-types'
 import cx from 'classnames'
 
-const Level = ({ type, variant, size }) => {
+const Level = ({ variant, size }) => {
   const styles = cx(
     'ola_level',
     variant && `is-${variant}`,
-    size === 'medium' ? null : `is-${size}`,
-    `is-${type}`
+    size === 'medium' ? null : `is-${size}`
   )
 
   return (
-    type === 'three_levels' ?
-      <span className={styles}></span> :
-      <div className={styles}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+    <div className={styles}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
   )
 }
 
 Level.defaultProps = {
-  type: 'three_levels',
   variant: null,
   size: 'medium',
 }
 
 Level.propTypes = {
-  /** Level types */
-  type: PT.oneOf(['three_levels', 'four_levels']),
   /** Level variants */
   variant: PT.oneOf(['low', 'medium', 'high', 'highest']),
   /** Level sizes */
