978f58403acd93bebacbbee6f073102db5caea0c
Diego Pascual <diego@marketgoo.com>
2021-06-02 18:26:14 +0200

    added arrowRight icon, add pending and done variant to Isue, changed TreeOptionList right icon

---
+++ src/Icon/ArrowRight.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const ArrowRight = () => <path
+  d="M26 14L16.708 3.293a1 1 0 00-1.5 1.319L22.5 13H3a1 1 0 00-.117 1.993L22.5 15l-7.207 8.292c-.36.36-.405 1.036-.02 1.408.386.37.946.395 1.339.09L26 14z" />
+
+export default ArrowRight
