31da2b1c2e19484c8cc437b67dc668544e8d3eeb
griselmatosm <griselmatosm@gmail.com>
2021-08-24 18:16:04 +0200

    added new props backgroundColor & borderColor into Table component

--- src/Table/Cell.js
+++ src/Table/Cell.js
@@ -2,10 +2,10 @@ import React from 'react'
 import {default as PT} from 'prop-types'
 import cx from 'classnames'
 
-const TableCell = ({ header, variant, className, children, ...props }) => {
+const TableCell = ({ header, variant, backgroundColor, borderColor, className, children, ...props }) => {
   const Component = header ? 'th' : 'td'
   return (
-    <Component className={cx('ola_tableCell', `is-${variant}`, className)} {...props}>
+    <Component className={cx('ola_tableCell', `is-${variant}`, `has-${backgroundColor}`, className)} style={{ '--table-background': backgroundColor, '--borderColor': borderColor }} {...props}>
       {children}
     </Component>
   )
@@ -21,6 +21,10 @@ TableCell.propTypes = {
   className: PT.string,
   /** Content type */
   variant: PT.oneOf(['left', 'center', 'right', 'numeric', 'action', 'check', 'multiline']),
+  /** Cell with background */
+  backgroundColor: PT.string,
+  /** Cell with borderColor bottom */
+  borderColor: PT.string,
   /** Whether is a header */
   header: PT.bool,
   /** Childen nodes */
