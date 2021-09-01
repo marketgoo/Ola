afe572f63bd64d6b492d66b642617a042bf75362
mablanco <mblanco@marketgoo.com>
2021-09-01 13:53:02 +0200

    add format prop to ChartLineAxis component to fix big numbers

--- src/ChartLine/Axis.js
+++ src/ChartLine/Axis.js
@@ -2,15 +2,14 @@ import React from 'react'
 import {default as PT} from 'prop-types'
 import cx from 'classnames'
 
-const ChartLineAxis = ({ max, steps, className }) => {
+const ChartLineAxis = ({ max, steps, className, format }) => {
   const styles = cx('ola_chartLine-axis', className)
 
   steps = Math.min(max, steps)
   const decimals = max < steps * steps ? 10 : 1
-
   const step = max / steps
   const children = new Array(steps + 1).fill(null)
-    .map((v, index) => <span key={index}>{Math.round(index * step * decimals) / decimals}</span>)
+    .map((v, index) => <span key={index}>{format(Math.round(index * step * decimals) / decimals)}</span>)
   
   return (
     <div className={styles}>
@@ -22,7 +21,8 @@ const ChartLineAxis = ({ max, steps, className }) => {
 ChartLineAxis.defaultProps = {
   steps: 4,
   max: null,
-  className: null
+  className: null,
+  format:(num)=> num
 }
 
 ChartLineAxis.propTypes = {
@@ -32,6 +32,8 @@ ChartLineAxis.propTypes = {
   steps: PT.number,
   /** Extra className */
   className: PT.string,
+  /** Function to return number */
+  format:PT.func
 }
 
 export default ChartLineAxis
