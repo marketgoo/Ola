dc638f8690763b2fa45b7b7b5e96b2cce9bfd320
pedroparra <pedroparra.correo@gmail.com>
2021-12-09 16:40:07 +0100

    Add forwardRef into input component

--- src/Field/stories.js
+++ src/Field/stories.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Field from './'
 import Input from '../Input'
+import Button from '../Button'
+
+import { useForm, Controller } from 'react-hook-form'
 
 export default {
   title: 'Field',
@@ -13,10 +16,52 @@ export default {
   },
 }
 
-export const Base = (args) => <Field {...args}>
-  <Input placeholder="Text placeholder" />
-</Field>
+export const Base = (args) => (
+  <Field {...args}>
+    <Input placeholder="Text placeholder" />
+  </Field>
+)
+
+export const reactHookFormsExample = () => {
+  const onSubmit = (data) => setData(data)
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm()
+
+  const [data, setData] = useState(null)
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name={'testInput'}
+          control={control}
+          defaultValue={'Default value'}
+          rules={{ required: 'illo k ase loko?' }}
+          render={({ field }) => (
+            <Field
+              id="field-test"
+              label={'Test input'}
+              error={!!errors?.testInput?.message}
+              description={
+                errors?.testInput?.message ? errors.testInput.message : ''
+              }
+            >
+              <Input {...field} />
+            </Field>
+          )}
+        />
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </form>
+      {data && <code>{JSON.stringify(data)}</code>}
+    </>
+  )
+}
 
 Base.argTypes = {
-  children: { control: { disable: true }}
+  children: { control: { disable: true } },
 }
