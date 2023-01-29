dc6435890316a2d7b31e30702c595da08a250376
David Moreno <linuxonrails@linuxonrails.com>
2023-01-29 19:52:20 +0100

    Fix images source from https://placehold.it/100x100 to https://via.placeholder.com/100 (where 100 was an example dimension)
    The site https://placehold.it seems to no longer return test images:
    
    > 404 Not Found
    >   Code: NoSuchBucket
    >   Message: The specified bucket does not exist
    >   BucketName: www.placeholder.com
    >   RequestId: HF4ZKXV9GDC5QAWX
    >   HostId: 4r62FQ9Ya4MGkzylGkTXeLTFYAMfbdxk/MVZq64QhIk1GCpjuEXo8FnUIosIfuxKkHnXcZyLfr4=

--- src/Thumbnail/test.js
+++ src/Thumbnail/test.js
@@ -3,7 +3,7 @@ import Thumbnail from './'
 import renderer from 'react-test-renderer'
 
 it('Default Thumbnail', () => {
-  const tree = renderer.create(<Thumbnail src="https://placehold.it/100x100" errorImage="https://via.placeholder.com/100/000000" />).toJSON()
+  const tree = renderer.create(<Thumbnail src="https://via.placeholder.com/100" errorImage="https://via.placeholder.com/100/000000" />).toJSON()
   expect(tree).toMatchSnapshot()
 })
 
