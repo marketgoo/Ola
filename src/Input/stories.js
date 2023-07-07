ec6c31c2e0e6c6bcfd11745bf260b9d7531f7b54
Luis Miguel <luis@marketgoo.com>
2023-07-07 07:45:48 +0200

    refactor: extend stories

--- src/Input/stories.js
+++ src/Input/stories.js
@@ -7,6 +7,11 @@ export default {
   args: {
     disabled: false,
     readOnly: false,
+    error: false,
+    placeholder: 'Text placeholder',
+    className: '',
+    type: 'text',
+    value: ''
   },
   argTypes: {
     onClick: { action: 'clicked' },
@@ -16,4 +21,50 @@ export default {
   },
 }
 
-export const Base = (args) => <Input {...args} />
+export const InputBase = (args) => <Input {...args} />
+
+export const InputError = (args) => <Input {...args} />
+InputError.args = {
+  error: true,
+}
+
+export const InputDisabled = (args) => <Input {...args} />
+InputDisabled.args = {
+  disabled: true,
+  value: 'Input disabled'
+}
+
+export const InputReadOnly = (args) => <Input {...args} />
+InputReadOnly.args = {
+  readOnly: true,
+  value: 'Input readonly'
+}
+
+export const TextareaBase = (args) => <Input {...args} />
+TextareaBase.args = {
+  placeholder: 'Textarea placeholder',
+  type: 'textarea',
+}
+
+export const TextareaError = (args) => <Input {...args} />
+TextareaError.args = {
+  placeholder: 'Textarea placeholder',
+  type: 'textarea',
+  error: true,
+}
+
+export const TextareaDisabled = (args) => <Input {...args} />
+TextareaDisabled.args = {
+  placeholder: 'Textarea placeholder',
+  type: 'textarea',
+  disabled: true,
+  value: 'Textarea disabled'
+}
+
+export const TextareaReadOnly = (args) => <Input {...args} />
+TextareaReadOnly.args = {
+  placeholder: 'Textarea placeholder',
+  type: 'textarea',
+  readOnly: true,
+  value: 'Textarea readonly'
+}
