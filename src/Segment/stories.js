59f808ff2b194fa8e812c9c5106ba5eead50d62e
Luis Miguel <luis@marketgoo.com>
2023-07-11 16:12:38 +0200

    feat: change styles and implementation of Segment component

--- src/Segment/stories.js
+++ src/Segment/stories.js
@@ -1,17 +1,34 @@
 import React from 'react'
-import Button from '../Button'
 import Segment from './'
 
 export default {
   title: 'Segment',
-  component: Segment
+  component: Segment,
+  args: {
+    className: '',
+  },
+  argTypes: {
+    children: {
+      control: { disable: true }
+    },
+    onClick: { action: 'clicked' },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '16px' }}>
+        <p style={{
+          backgroundColor: 'var(--color-neutral-100)',
+          padding: '16px',
+          marginBottom: '32px',
+        }}>Include each button as <code>&lt;Segment.Button&gt;</code>. Is <b>NOT necessary</b> to use and import <code>&lt;SegmentButton&gt;</code> directly</p>
+        {Story()}
+      </div>
+    ),
+  ],
 }
 
 export const Base = (args) => <Segment {...args}>
-  <Button selected variant='secondary'>All keywords</Button>
-  <Button selected={false} variant='secondary'>Only focus keywords</Button>
+  <Segment.Button selected>One &lt;Segment.Button&gt;</Segment.Button>
+  <Segment.Button>I am a button</Segment.Button>
+  <Segment.Button as="a" href="https://www.marketgoo.com/" target="_blank">I am an anchor</Segment.Button>
 </Segment>
-
-Base.argTypes = {
-  children: { control: { disable: true }}
-}
