7c2e60975ecf78c8dc5737bb21144d941a7e5f96
Luis Miguel <luis@marketgoo.com>
2023-07-12 17:34:37 +0200

    feat: change implementation and styles of ButtonIcon

--- src/ButtonIcon/index.js
+++ src/ButtonIcon/index.js
@@ -2,48 +2,73 @@ import React from 'react'
 import {default as PT} from 'prop-types'
 import { getElementType } from '../utils'
 import cx from 'classnames'
-
+import Icon from '../Icon'
 import Spinner from '../Spinner'
 
-const ButtonIcon = ({ as, className, children, busy, disabled, variant, ...props }) => {
+const ButtonIcon = ({ as, className, busy, disabled, color, variant, invert, icon, children, ...props }) => {
   const ElementType = getElementType(ButtonIcon, { as: as, ...props })
-  delete props['as']
+  delete props.as
+
+  const variantNumber = variant === 'normal' ? '500' : '900'
+
+  const iconElement = typeof icon === 'string' ? <Icon name={icon} /> : icon
+
   const styles = cx(
-    'ola_buttonIcon',
+    'ola_button-icon',
     `is-${variant}`,
     {'is-busy': busy},
     {'is-disabled': disabled && !busy},
+    {'is-invert': invert},
     className
   )
   return (
-    <ElementType {...props} disabled={busy ? true : disabled} className={styles}>
-      {busy ? <Spinner /> : children}
+    <ElementType
+      className={styles}  
+      disabled={busy || disabled}
+      style={{
+        '--base-color': `var(--color-${color}-${variantNumber})`,
+        '--busy-color': `var(--color-${color}-600)`,
+        '--disabled-color': `var(--color-${color}-300)`,
+        '--shadow-focus': `var(--shadow-focus-${color})`,
+      }}
+      {...props}>
+      {busy ? <Spinner /> : iconElement}
     </ElementType>
   )
 }
 
 ButtonIcon.defaultProps = {
   as: 'button',
-  variant: 'primary',
+  variant: 'normal',
+  color: 'neutral',
   busy: false,
-  disabled: false
+  disabled: false,
+  invert: false,
+  icon: null
 }
 
 ButtonIcon.propTypes = {
+  /** Name of the icon (see Ola Icon) or a svg direcly */
+  icon: PT.oneOf([
+    PT.string,
+    PT.shape({
+      type: PT.oneOf(['svg']),
+    })
+  ]),
   /** Render ButtonIcon with any html tag */
   as: PT.string,
-  /** Button variants */
-  variant: PT.oneOf(['primary', 'secondary', 'destructive', 'chevron']),
+  /** Icon color */
+  color: PT.oneOf(['primary', 'neutral', 'positive', 'negative', 'warning', 'pro', 'accent']),
+  /** Icon variant (normal=color-500, dark=color-900) */
+  variant: PT.oneOf(['normal', 'dark']),
   /** Extra className */
   className: PT.string,
-  /** Childen nodes */
-  children: PT.oneOfType([
-    PT.string,
-    PT.arrayOf(PT.node),
-    PT.node
-  ]).isRequired,
+  /** Button busy state */
   busy: PT.bool,
+  /** Button disabled state */
   disabled: PT.bool,
+  /** Indicate if icon should have color white */
+  invert: PT.bool,
 }
 
 export default ButtonIcon
