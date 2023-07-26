783512e820fd76cef74eb81576d717fb9ccb534b
Luis Miguel <luis@marketgoo.com>
2023-07-26 13:22:11 +0200

    feat: include new icons

---
+++ src/Icon/ArrowDownFill.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ArrowDownFill = () => <path fill="currentColor" d="M4.557 12.463A.75.75 0 0 1 5.25 12h13.5a.75.75 0 0 1 .53 1.28l-6.75 6.75a.747.747 0 0 1-1.06 0l-6.75-6.75a.75.75 0 0 1-.163-.817Z"/>
+
+export default ArrowDownFill
